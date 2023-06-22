0ced075bfcdbb644292fab51e75a457bff750d66
KGr1sha <krylov.grigory@gmail.com>
2023-06-22 23:54:26 +0300

    WATAFUK

--- Assets/Scripts/BobMovement.cs
+++ Assets/Scripts/BobMovement.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class BobMovement : MonoBehaviour
 {
     private float horizontal;
+    private Rigidbody2D rb;
     [SerializeField] private float movespeed = 2f;
-    
-    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        
+        rb.MovePosition(new Vector2(rb.position.x + horizontal * movespeed * Time.deltaTime, rb.position.y));
     }
 }

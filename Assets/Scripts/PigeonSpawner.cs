a941ad7e701af8140e34a8e64f166fefaa870a27
KGr1sha <krylov.grigory@gmail.com>
2023-06-23 11:29:36 +0300

    pigeon movement

---
+++ Assets/Scripts/PigeonSpawner.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject pigeonPrefab;
+    [SerializeField] private float spawnRate;
+}

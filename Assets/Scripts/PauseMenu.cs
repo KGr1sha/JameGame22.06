5b0e8012a0721ce66d1fa9311ec1b5ba3515f122
Rustam12321342334123 <tomatut1155@yandex.ru>
2023-06-24 16:17:59 +0300

    pshhhhhh

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -12,10 +12,7 @@ public class PauseMenu : MonoBehaviour {
     void Update  () {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            } else
+            if (GameIsPaused == false)
             {
                 Pause();
             }

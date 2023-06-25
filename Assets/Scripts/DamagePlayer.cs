731d8c26941d0a5ce44f275096acd6aabf3521ba
KGr1sha <krylov.grigory@gmail.com>
2023-06-25 16:49:29 +0300

    orel damage adjust

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
+    [SerializeField] private int damage = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            collision.gameObject.GetComponent<BobLife>().TakeDamage(1);
+            collision.gameObject.GetComponent<BobLife>().TakeDamage(damage);
         }
     }
 }

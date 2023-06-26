b60e2b98152c4b5f4771d705214a2b53da74b349
KGr1sha <krylov.grigory@gmail.com>
2023-06-26 17:06:05 +0300

    pls work

--- Assets/Scripts/SetSkins.cs
+++ Assets/Scripts/SetSkins.cs
@@ -4,8 +4,57 @@ using UnityEngine;
 
 public class SetSkins : MonoBehaviour
 {
+    [SerializeField] GameObject bob;
+    private SpriteRenderer bobSprite;
+
+    [Header("SKINS")]
+    [SerializeField] Sprite defaultSkin;
+    [SerializeField] Sprite halal;
+    [SerializeField] Sprite monkey;
+    [SerializeField] Sprite money;
+    [SerializeField] Sprite shit;
+    [SerializeField] Sprite notabean;
+
+
+
+
+    private void Awake()
+    {
+        string chosenSkin = GetSkin();
+        switch (chosenSkin)
+        {
+            case "default":
+                bobSprite.sprite = defaultSkin;
+                break;
+            case "halal":
+                bobSprite.sprite = halal;
+                break;
+            case "monkey":
+                bobSprite.sprite = monkey;
+                break;
+            case "money":
+                bobSprite.sprite = money;
+                break;
+            case "shit":
+                bobSprite.sprite = shit;
+                break;
+            case "notabean":
+                bobSprite.sprite = notabean;
+                break;
+        }
+    }
+
+
+
     public void SetPlayerSkin(string skin)
     {
         PlayerPrefs.SetString("skin", skin);
     }
+
+    private string GetSkin()
+    {
+        return PlayerPrefs.GetString("skin");
+    }
+
+
 }
